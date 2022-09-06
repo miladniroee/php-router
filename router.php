d0f7244e1da38d51bd0b7fe7b59afc60b84d0bba
Milad Remote <mniroee@gmail.com>
2022-09-06 23:05:21 +0430

    add router file

---
+++ router.php
@@ -0,0 +1,56 @@
+<?php
+
+class Route
+{
+    private static string $Url;
+
+    public static function get($Url, $Params)
+    {
+        self::$Url = $Url;
+        if ($_SERVER['REQUEST_METHOD'] === 'GET' && self::CheckURL()) {
+            self::InitController($Params);
+        }
+    }
+
+    public static function post($Url, $Params)
+    {
+        self::$Url = $Url;
+
+        if ($_SERVER['REQUEST_METHOD'] === 'POST' && self::CheckURL()) {
+            self::InitController($Params);
+        }
+    }
+
+    private static function InitController($Params)
+    {
+        $Params = explode('@', $Params);
+        $Controller = $Params[0];
+        $Method = $Params[1];
+
+        require_once __DIR__ . "/controller/{$Controller}.php";
+        $ClassInit = new $Controller();
+        $ClassInit->$Method();
+        if (property_exists($ClassInit, 'Scripts')):
+            $_SESSION['Scripts'] = $ClassInit->Scripts;
+        else:
+            $_SESSION['Scripts'] = [];
+        endif;
+    }
+
+    private static function CheckURL():bool
+    {
+        $Uri = array_values(array_filter(explode('/', self::$Url)));
+        $Url = array_filter(explode('/', $_GET['param']));
+        foreach ($Uri as $Key => $Params):
+            if (preg_match("/\{(.*?)\}/", $Params)):
+                continue;
+            else:
+                if ($Params !== $Url[$Key]):
+                    return false;
+                endif;
+            endif;
+        endforeach;
+        return true;
+    }
+
+}
